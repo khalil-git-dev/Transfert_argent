a2ec9f3c409d5a7808a287133c726585ff3ff083
khalil-git-dev <diopfaizine@gmail.com>
2020-01-01 13:44:51 +0000

    initialisation_projet

---
+++ src/Controller/UtilisateurController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Utilisateur;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class UtilisateurController extends AbstractController
+{
+    /**
+     * @Route("/utilisateur", name="utilisateur")
+     */
+    public function index()
+    {
+        return $this->render('utilisateur/index.html.twig', [
+            'controller_name' => 'UtilisateurController',
+        ]);
+    }
+
+    /**
+     * @Route("/login", name="login", methods={"POST"})
+     */
+    public function login(Request $request)
+    {
+        $user = new Utilisateur();
+        $user = $this->getUtilisateur();
+        return $this->json([
+            'email' => $user->getEmail(),
+            'roles' => $user->getRoles()
+        ]);
+    }
+    
+}
