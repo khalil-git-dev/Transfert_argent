13946bcf66d921b35694585f1b4c4bfc6c517edd
khalil-git-dev <diopfaizine@gmail.com>
2020-01-06 14:16:08 +0000

    ajout_users

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -2,36 +2,72 @@
 
 namespace App\Controller;
 
+use App\Entity\Profile;
 use App\Entity\Utilisateur;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
+/**
+ * @Route("/api")
+*/
 class AuthController extends AbstractController
 {
     /**
-     * @Route("/auth", name="auth")
+     * @Route("/enregistrer", name="enregistrer_user", methods={"POST"})
      */
-    public function register(Request $request, UserPasswordEncoderInterface $encoder)
+    public function enregistrer(Request $request, UserPasswordEncoderInterface $passwordEncoder, EntityManagerInterface $entityManager, SerializerInterface $serializer, ValidatorInterface $validator)
     {
-        // $em = $this->getDoctrine()->getManager();
+        $values = json_decode($request->getContent());
+        if(isset($values->username,$values->password)) {
+            $user = new Utilisateur();
+            $user->setEmail($values->username);
+            $user->setPassword($passwordEncoder->encodePassword($user, $values->password));
+            
+            // recuperer id profil  
+            $reposit = $this->getDoctrine()->getRepository(Profile::class);
+            $profils = $reposit->find($values->libelle);
+            $user->setRole($profils);
+            $role = [];
+            if ($profils->getLibelle() == "ROLE_ADMIN") {
+                $role = (["ROLE_ADMIN"]);
+            } elseif ($profils->getLibelle() == "ROLE_CAISSIER") {
+                $role = (["ROLE_CAISSIER"]);
+            } elseif ($profils->getLibelle() == "ROLE_ADMIN_SYSTEM") {
+                $role = (["ROLE_ADMIN_SYSTEM"]);
+            }
+            
+            $user->setRoles($role);
+            $user->setLogin($values->login);
+            $user->setEmail($values->email);
+           
+            $errors = $validator->validate($user);
+            if(count($errors)) {
+                $errors = $serializer->serialize($errors, 'json');
+                return new Response($errors, 500, [
+                    'Content-Type' => 'application/json'
+                ]);
+            }
+            $entityManager->persist($user);
+            $entityManager->flush();
 
-        // $email = $request->request->get('khalildiop@gmail.com');
-        // $password = $request->request->get('khalil');
-        // $roles = $request->request->get('role_sup_admin');
+            $data = [
+                'status' => 201,
+                'message' => 'L\'utilisateur a été créé'
+            ];
 
-        // if (!$roles) {
-        //     $roles = json_encode([]);
-        // }
-
-        // $user = new Utilisateur($email);
-        // $user->setPassword($encoder->encodePassword($user, $password));
-        // $user->setRoles(($roles));
-        // $em->persist($user);
-        // $em->flush();
-
-        // return new Response(sprintf('User %s successfully created', $user->getUsername()));
+            return new JsonResponse($data, 201);
+        }
+        $data = [
+            'status' => 500,
+            'message' => 'Vous devez renseigner les clés username et password'
+        ];
+        return new JsonResponse($data, 500);
     }
 }
